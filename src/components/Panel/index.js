e518e95c3809f9b04eb5ac51bbd45cefc0f051a5
somytaheri <somytaheri@users.noreply.github.com>
2019-07-28 17:50:31 +1000

    Use hook instead of class

--- src/components/Panel/index.js
+++ src/components/Panel/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
@@ -7,48 +7,41 @@ import axios from "axios";
 import DataTable from "../DataTable";
 import ProfitTable from "../ProfitTable";
 import calculateProfit from "./logic";
-
-class Panel extends React.Component {
-  state = {
-    currencies: [],
-    profit: []
-  };
-
-  handleProfitCalculate = () => {
-    const profit = calculateProfit(this.state.currencies);
-    this.setState({
-      profit
-    });
-  };
-
-  async componentDidMount() {
-    let { data: currencies } = await axios.get("data.json");
-    this.setState({ currencies });
-  }
-
-  render() {
-    const { currencies, profit } = this.state;
-    return (
-      <Container maxWidth="xl">
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={8} md={6}>
-            <Paper boxShadow={0}>
-              {" "}
-              <DataTable
-                data={currencies}
-                handleProfitCalculate={this.handleProfitCalculate}
-              />
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={4} md={3}>
-            <Paper boxShadow={0}>
-              <ProfitTable result={profit} />
-            </Paper>
-          </Grid>
-        </Grid>
-      </Container>
+const Panel = () => {
+  const [currencies, setCurrencies] = useState([]);
+  const [profit, setProfit] = useState([]);
+  const handleProfitCalculate = currency => {
+    let selectedCurrency = currencies.filter(
+      item => item.currency === currency
     );
-  }
-}
+    let profit = calculateProfit(selectedCurrency);
+    setProfit(profit);
+  };
+
+  useEffect(async () => {
+    let { data: currencies } = await axios.get("data.json");
+    setCurrencies(currencies);
+  }, []);
+
+  return (
+    <Container maxWidth="xl">
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={8} md={6}>
+          <Paper>
+            <DataTable
+              data={currencies}
+              handleProfitCalculate={handleProfitCalculate}
+            />
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={4} md={3}>
+          <Paper>
+            <ProfitTable result={profit} />
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
 export default Panel;
