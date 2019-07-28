fd0fbbb1757a92e5a9de24c982e4831c3a56c63d
somytaheri <somytaheri@users.noreply.github.com>
2019-07-28 20:16:54 +1000

    update readme

---
+++ build/precache-manifest.5414d78607c1ca2e08c782f0e194f704.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63e0f3c2fe4cd0ea620a8aba53c644c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab0750a028dca5ddd032",
+    "url": "/static/js/2.c5f6c1e1.chunk.js"
+  },
+  {
+    "revision": "532f4e96c97f32782a7d",
+    "url": "/static/js/main.da914ef9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
